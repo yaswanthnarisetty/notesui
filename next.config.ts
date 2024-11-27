60fb93c3cee5aae27c9bda045954fb8cb072ebe8
yaswanthnarisetty <yaswanthyuga@gmail.com>
2024-11-27 22:52:12 +0530

    conf

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
